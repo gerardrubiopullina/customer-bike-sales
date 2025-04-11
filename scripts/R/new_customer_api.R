9f3db9c5c4a1fe4fd6c7f045321e6fbf55e3cf14
Gerard <gerard.rubio2000@gmail.com>
2025-04-11 16:12:23 +0200

    get medoids distance api

--- scripts/R/new_customer_api.R
+++ scripts/R/new_customer_api.R
@@ -1,4 +1,9 @@
 library(plumber)
+library(proxy)
+library(cluster)
+
+# Load the PAM model
+pam_model <- readRDS("pam_model.rds")
 
 #* @apiTitle Customer Classification API
 
@@ -21,18 +26,56 @@ function(req, res) {
 #* @param customer The customer data
 #* @post /classify
 function(customer) {
-  required_fields <- c("firstName", "lastName", "gender")
+  required_fields <- c(
+    "firstName", "lastName", "gender", "maritalStatus",
+    "age", "yearlyIncome", "avgMonthSpend",
+    "numberCarsOwned", "numberChildrenAtHome"
+  )
   missing_fields <- required_fields[!required_fields %in% names(customer)]
 
   if (length(missing_fields) > 0) {
     return(list(
-      error = paste("Campos obligatorios faltantes:", paste(missing_fields, collapse = ", ")) # nolint
+      error = paste("Missing required fields:", paste(missing_fields, collapse = ", ")) # nolint
     ))
   }
 
+  # Extract medoids from the PAM model
+  medoids_indices <- pam_model$medoids
+  medoids_data <- pam_model$data[medoids_indices, ]
+
+  # Prepare customer data in the same format as the training data
+  new_customer <- list(
+    CountryRegionName = "",
+    Education = "",
+    Occupation = "",
+    Gender = customer$gender,
+    MaritalStatus = customer$maritalStatus,
+    HomeOwnerFlag = 1,
+    NumberCarsOwned = as.integer(customer$numberCarsOwned),
+    NumberChildrenAtHome = as.integer(customer$numberChildrenAtHome),
+    TotalChildren = as.integer(customer$numberChildrenAtHome),
+    YearlyIncome = as.numeric(customer$yearlyIncome),
+    Age = as.integer(customer$age)
+  )
+
+  # Convert to data frame
+  new_customer_df <- as.data.frame(new_customer, stringsAsFactors = TRUE)
+
+  # Calculate Gower distance to each medoid
+  gower_dist <- proxy::dist(new_customer_df, medoids_data, method = "gower")
+
+  # Find the closest medoid
+  cluster_assignment <- which.min(as.matrix(gower_dist)[1,])
+
   result <- list(
-    message = paste0(customer$firstName, " ", customer$lastName, " is a ",
-      ifelse(customer$gender == "M", "male", "female"), ".") # nolint
+    message = paste0(customer$firstName, " ", customer$lastName, " has been classified to cluster ", cluster_assignment),
+    cluster = as.integer(cluster_assignment),
+    clusterProbability = list(
+      cluster1 = round(1 - (as.matrix(gower_dist)[1,1] / sum(as.matrix(gower_dist)[1,])), 2),
+      cluster2 = round(1 - (as.matrix(gower_dist)[1,2] / sum(as.matrix(gower_dist)[1,])), 2),
+      cluster3 = round(1 - (as.matrix(gower_dist)[1,3] / sum(as.matrix(gower_dist)[1,])), 2)
+    ),
+    bikeBuyerProbability = ifelse(cluster_assignment == 3, "High", ifelse(cluster_assignment == 1, "Medium", "Low"))
   )
   result
 }
